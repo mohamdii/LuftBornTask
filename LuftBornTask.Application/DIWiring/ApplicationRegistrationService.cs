a155494e6bc6a238eaab56d1a7386656a9b5b47a
mohamdii <m.hamdy97@gmail.com>
2026-09-02 21:45:39 +0300

    Clean up usings and reformat code
    Removed unused using directives and combined some onto a single line for clarity. Noted a redundant IProductService registration with no functional changes. No new services were added.

--- LuftBornTask.Application/DIWiring/ApplicationRegistrationService.cs
+++ LuftBornTask.Application/DIWiring/ApplicationRegistrationService.cs
@@ -1,10 +1,7 @@
 ﻿using LuftBornTask.Application.Interfaces;
 using LuftBornTask.Application.Services;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
 using System.Reflection;
-using System.Text;
 
 namespace LuftBornTask.Application.DIWiring
 {
@@ -13,7 +10,7 @@ namespace LuftBornTask.Application.DIWiring
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddAutoMapper(cfg => { }, Assembly.GetExecutingAssembly());
-            services.AddScoped<IProductService, ProductService>();  
+            services.AddScoped<IProductService, ProductService>();
             // Register application services here
             // Example: services.AddScoped<IProductService, ProductService>();
             return services;
