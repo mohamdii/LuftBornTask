ae1c0ea072cdc50fe50befa676dc3e2093110c2f
mohamdii <m.hamdy97@gmail.com>
2026-09-02 21:40:51 +0300

    Add ApplicationRegistrationService for DI setup
    Added ApplicationRegistrationService with AddApplication extension method to register AutoMapper and IProductService for dependency injection.

---
+++ LuftBornTask.Application/DIWiring/ApplicationRegistrationService.cs
@@ -0,0 +1,22 @@
+﻿using LuftBornTask.Application.Interfaces;
+using LuftBornTask.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace LuftBornTask.Application.DIWiring
+{
+    public static class ApplicationRegistrationService
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(cfg => { }, Assembly.GetExecutingAssembly());
+            services.AddScoped<IProductService, ProductService>();  
+            // Register application services here
+            // Example: services.AddScoped<IProductService, ProductService>();
+            return services;
+        }
+    }
+}
