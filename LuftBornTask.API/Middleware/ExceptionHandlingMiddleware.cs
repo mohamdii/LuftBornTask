e82cdf34e8b9f733c60b89c7d624c8468f59a678
mohamdii <m.hamdy97@gmail.com>
2026-09-02 17:06:55 +0300

    Refactor ExceptionHandlingMiddleware location and code
    Moved ExceptionHandlingMiddleware from API to Infrastructure project, simplified its implementation, and updated csproj to remove explicit Middleware folder reference.

---
+++ LuftBornTask.API/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuftBornTask.API.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+    }
+}
---
+++ LuftBornTask.Infrastructure/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuftBornTask.Infrastructure.Middleware
+{
+    public  class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+    }
+}
